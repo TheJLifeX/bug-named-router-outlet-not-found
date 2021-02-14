00d128ea60904356460ac760787c69775bc3f5ce
TheJLifeX <44620382+TheJLifeX@users.noreply.github.com>
2021-02-14 19:29:43 +0100

    Minimal reproduction.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,22 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    outlet: 'test',
+    path: 'test',
+    loadChildren: () => import('./test-page/test-page.module').then(m => m.TestPageModule)
+  },
+  {
+    outlet: 'test',
+    path: '**',
+    loadChildren: () => import('./page-not-found/page-not-found.module').then(m => m.PageNotFoundModule)
+  },
+  {
+    path: '**',
+    loadChildren: () => import('./page-not-found/page-not-found.module').then(m => m.PageNotFoundModule)
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
